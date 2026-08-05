c0a4c714821aad74ef772bb3b4b61c1dab34f233
Varun Kumar <varunkumarvelpula949@gmail.com>
2026-08-05 23:12:31 +0530

    fix vercel backend deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 app.post("/contact", (req, res) => {
   const { name, email, message } = req.body;
 
