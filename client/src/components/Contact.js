02acacf982bd993204a1aada1e7505e63f8bf488
Varun Kumar <varunkumarvelpula949@gmail.com>
2026-08-05 22:32:29 +0530

    fix contact api url

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -20,7 +20,7 @@ function Contact() {
 
     try {
       const res = await axios.post(
-        "http://axios.post("https:axios.post("https://fullstackportfolio11-p283.vercel.app/...")
+        "http://fullstackportfolio11-p283.vercel.app/contact")
         form
       );
 
